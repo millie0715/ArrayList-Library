36a2dc983e13daeb6db8211f8741ef0dac0f6af1
millie0715 <111794954+millie0715@users.noreply.github.com>
2023-02-05 18:31:15 -0800

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,7 +1,5 @@
 package arrayList_library;
-
 import java.util.Scanner;;
-
 public class Driver {
 	public static void main(String[] args) {
 		BookList list = new BookList();
@@ -10,23 +8,12 @@ public class Driver {
 		Scanner input = new Scanner(System.in);
 		int number = input.nextInt();
 		while(number > 0) {
-			if(number == 1) {
-				list.listAllBooks();}
-			else if(number == 2) {
-				list.listAllBooksByPublicationDate();}
-			else if(number == 3) {
-				list.listAllBooksByLength();}
-			else if(number == 4) {
-				list.listAllBooksByRating();}
-			else if(number == 5) {
-				list.sortBooksWithSubject();}
-			else if(number == 6) {
-				list.listAllDetailsForABook();}
-			else if(number == 7) {
-				list.addBook();}
-			else if(number == 8) {
-				break;}
-			number = input.nextInt();
-		}
-	}
-}
+			if(number == 1) {list.listAllBooks();}
+			else if(number == 2) {list.listAllBooksByPublicationDate();}
+			else if(number == 3) {list.listAllBooksByLength();}
+			else if(number == 4) {list.listAllBooksByRating();}
+			else if(number == 5) {list.sortBooksWithSubject();}
+			else if(number == 6) {list.listAllDetailsForABook();}
+			else if(number == 7) {list.addBook();}
+			else if(number == 8) {break;}
+			number = input.nextInt();}}}
